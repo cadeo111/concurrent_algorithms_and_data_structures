509eb66293f38c37eaf13dcc0116e1cf47becdb2
cadenkeese <caden@keese.dev>
2024-12-03 05:25:40 +0000

    moved to shared ptr, and coarse set works

--- lab1/lab01/src/coarse_set.hpp
+++ lab1/lab01/src/coarse_set.hpp
@@ -5,6 +5,8 @@
 
 #include <mutex>
 
+#include "simple_set.hpp"
+
 /// The node used for the linked list implementation of a set in the [`CoarseSet`]
 /// class. This struct is used for task 3
 struct CoarseSetNode {
@@ -14,17 +16,17 @@ struct CoarseSetNode {
 };
 
 /// A set implementation using a linked list with coarse grained locking.
-class CoarseSet: public Set {
+class CoarseSet : public Set {
 private:
     // A03: You can add or remove fields as needed. Just having the `head`
     // pointer and the `lock` should be sufficient for task 3
-    std::unique_ptr<CoarseSetNode> head;
+    SimpleSetImpl unsafe_set;
     std::mutex lock;
-    EventMonitor<CoarseSet, StdSet, SetOperator>* monitor;
+    EventMonitor<CoarseSet, StdSet, SetOperator> *monitor;
+
 public:
-    CoarseSet(EventMonitor<CoarseSet, StdSet, SetOperator>* monitor) :
-        monitor(monitor)
-    {
+    CoarseSet(EventMonitor<CoarseSet, StdSet, SetOperator> *monitor) : monitor(monitor),
+                                                                       unsafe_set(SimpleSetImpl("CoarseSet")) {
         // A03: Initiate the internal state
     }
 
@@ -32,46 +34,43 @@ public:
         // A03: Cleanup any memory that was allocated
     }
 
-    bool _add_unsafe(int elem) {
-
-
-
-    }
 
     bool add(int elem) override {
         lock.lock();
-        bool result = _add_unsafe(elem);
-        lock.unlock();
-
+        bool result = unsafe_set.add(elem);
         // A03: Add code to insert the element into the set and update `result`.
         //      Also make sure, to insert the event inside the locked region of
         //      the linearization point.
         this->monitor->add(SetEvent(SetOperator::Add, elem, result));
+        lock.unlock();
         return result;
     }
 
     bool rmv(int elem) override {
-        bool result = false;
+        lock.lock();
+        bool result = unsafe_set.rmv(elem);
         // A03: Add code to remove the element from the set and update `result`.
         //      Also make sure, to insert the event inside the locked region of
         //      the linearization point.
         this->monitor->add(SetEvent(SetOperator::Remove, elem, result));
+        lock.unlock();
         return result;
     }
 
     bool ctn(int elem) override {
-        bool result = false;
+        lock.lock();
+        bool result = unsafe_set.ctn(elem);
         // A03: Add code to check if the element is inside the set and update `result`.
         //      Also make sure, to insert the event inside the locked region of
         //      the linearization point.
         this->monitor->add(SetEvent(SetOperator::Contains, elem, result));
+        lock.unlock();
         return result;
     }
 
     void print_state() override {
         // A03: Optionally, add code to print the state. This is useful for debugging,
         // but not part of the assignment
-        std::cout << "CoarseSet {...}";
+        unsafe_set.print_state();
     }
 };
-
--- lab1/lab01/src/simple_set.hpp
+++ lab1/lab01/src/simple_set.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "set.hpp"
+
+#include <utility>
 #include "std_set.hpp"
 
 /// The node used for the linked list implementation of a set in the [`SimpleSet`]
@@ -8,68 +10,54 @@
 struct SimpleSetNode {
     // A02: You can add or remove fields as needed.
     int value;
-    SimpleSetNode *next;
+    std::shared_ptr<SimpleSetNode> next;
 };
 
-/// A simple set implementation using a linked list. This class shouldn't have
-// any synchronization yet.
-class SimpleSet : public Set {
+class SimpleSetImpl {
 private:
-    // A02: You can add or remove fields as needed. Just having the `head`
-    // pointer should be sufficient for task 2
-    SimpleSetNode *head;
-    EventMonitor<SimpleSet, StdSet, SetOperator> *monitor;
+    std::shared_ptr<SimpleSetNode> head;
+    std::string label;
 
 public:
-    explicit SimpleSet(EventMonitor<SimpleSet, StdSet, SetOperator> *monitor) : monitor(monitor), head(nullptr) {
-        // A02: Initiate the internal state
+    explicit SimpleSetImpl(std::string label)
+        : label(std::move(label)) {
     }
 
-    ~SimpleSet() override {
-        // A02: Cleanup any memory that was allocated
-        _del_next(head);
-        head = nullptr;
-    }
+    ~SimpleSetImpl() = default;
 
-    static void _del_next(SimpleSetNode *node) {
-        if (node->next != nullptr) {
-            _del_next(node->next);
-            delete node;
-        }
-    }
-
-    bool _add(int elem) {
+    bool add(int elem) {
         // std::cout << "adding " << elem << "\n";
         // If the list is empty add the value and return true
         if (this->head == nullptr) {
-            this->head = new SimpleSetNode{
+            this->head = std::make_shared<SimpleSetNode>(SimpleSetNode{
                 .value = elem,
                 .next = nullptr
-            };
+            });
             return true;
         }
         // one element ie [2]
         if (this->head->value == elem) {
             return false;
         }
+        // if the current head value is more than
         if (this->head->value > elem) {
-            const auto el = new SimpleSetNode{
+            // std::unique_ptr<SimpleSetNode> el = ;
+            this->head = std::make_shared<SimpleSetNode>(SimpleSetNode{
                 .value = elem,
-                .next = this->head
-            };
-            this->head = el;
+                .next = std::move(this->head)
+            });
             return true;
         }
         if (this->head->next == nullptr) {
-            this->head->next = new SimpleSetNode{
+            this->head->next = std::make_shared<SimpleSetNode>(SimpleSetNode{
                 .value = elem,
-                .next = this->head
-            };
+                .next = nullptr
+            });
             return true;
         }
         // 2+ elements
-        SimpleSetNode *prev = this->head;
-        SimpleSetNode *next = this->head->next;
+        auto prev = this->head;
+        auto next = this->head->next;
         // find the smallest value larger than elem, or the end of the list
         while (next != nullptr && elem > next->value) {
             prev = next;
@@ -77,33 +65,24 @@ public:
         }
         if (next == nullptr) {
             // there isn't any value larger than elem
-            prev->next = new SimpleSetNode{
+            prev->next = std::make_shared<SimpleSetNode>(SimpleSetNode{
                 .value = elem,
                 .next = nullptr
-            };
+            });
             return true;
         }
         if (next->value == elem) {
             return false;
         }
 
-        prev->next = new SimpleSetNode{
+        prev->next = std::make_shared<SimpleSetNode>(SimpleSetNode{
             .value = elem,
             .next = next
-        };
+        });
         return true;
     }
 
-
-    bool add(int elem) override {
-        // A02: Add code to insert the element into the set and update `result`.
-        bool result = this->_add(elem);
-        // validate_in_order();
-        this->monitor->add(SetEvent(SetOperator::Add, elem, result));
-        return result;
-    }
-
-    bool _rmv(int elem) {
+    bool rmv(int elem) {
         // std::cout << "removing " << elem << "\n";
         if (this->head == nullptr) {
             return false;
@@ -111,9 +90,7 @@ public:
 
         // one element ie [2]
         if (this->head->value == elem) {
-            const SimpleSetNode *h = this->head;
-            this->head = h->next;
-            delete h;
+            this->head = this->head->next;
             return true;
         }
         if (this->head->next == nullptr) {
@@ -121,8 +98,8 @@ public:
         }
 
         // 2+ elements
-        SimpleSetNode *prev = this->head;
-        SimpleSetNode *current = this->head->next;
+        auto prev = this->head;
+        auto current = this->head->next;
         // find the smallest value larger than elem, or the end of the list
         while (current != nullptr && elem != current->value) {
             prev = current;
@@ -133,22 +110,12 @@ public:
             return false;
         }
         // found elem
-        const SimpleSetNode *toDelete = current;
         prev->next = current->next;
-        delete toDelete;
         return true;
     }
 
-    bool rmv(int elem) override {
-        bool result = _rmv(elem);
-        // validate_in_order();
-        // A02: Add code to remove the element from the set and update `result`.
-        this->monitor->add(SetEvent(SetOperator::Remove, elem, result));
-        return result;
-    }
-
-    bool _ctn(int elem) {
-        SimpleSetNode *node = this->head;
+    bool ctn(const int elem) {
+        auto node = this->head;
         while (true) {
             if (node->value == elem) return true;
             if (node->next == nullptr) return false;
@@ -157,17 +124,10 @@ public:
         }
     }
 
-    bool ctn(int elem) override {
-        bool result = _ctn(elem);
-        // A02: Add code to check if the element is inside the set and update `result`.
-        this->monitor->add(SetEvent(SetOperator::Contains, elem, result));
-        return result;
-    }
-
     void validate_in_order() {
         if (this->head->next == nullptr) return;
         int last = this->head->value;
-        SimpleSetNode *node = this->head->next;
+        auto node = this->head->next;
         while (node != nullptr) {
             if (last >= node->value) {
                 this->print_state();
@@ -178,11 +138,11 @@ public:
         }
     }
 
-    void print_state() override {
+    void print_state() const {
         // A02: Optionally, add code to print the state. This is useful for debugging,
         // but not part of the assignment.
-        std::cout << "SimpleSet {";
-        SimpleSetNode *node = this->head;
+        std::cout << label << " {";
+        auto node = this->head;
         while (node != nullptr) {
             std::cout << node->value;
             if (node->next != nullptr) {
@@ -191,5 +151,52 @@ public:
             node = node->next;
         }
         std::cout << "}";
+    }
+};
+/// A simple set implementation using a linked list. This class shouldn't have
+// any synchronization yet.
+class SimpleSet : public Set {
+private:
+    // A02: You can add or remove fields as needed. Just having the `head`
+    // pointer should be sufficient for task 2
+    SimpleSetImpl set;
+    EventMonitor<SimpleSet, StdSet, SetOperator> *monitor;
+
+public:
+    explicit SimpleSet(EventMonitor<SimpleSet, StdSet, SetOperator> *monitor) : set(SimpleSetImpl("SimpleSet")), monitor(monitor) {
+        // A02: Initiate the internal state
+    }
+
+    ~SimpleSet() override = default;
+
+    bool add(int elem) override {
+        // A02: Add code to insert the element into the set and update `result`.
+        bool result = set.add(elem);
+        // validate_in_order();
+        this->monitor->add(SetEvent(SetOperator::Add, elem, result));
+        return result;
+    }
+
+
+    bool rmv(const int elem) override {
+        bool result = set.rmv(elem);
+        // validate_in_order();
+        // A02: Add code to remove the element from the set and update `result`.
+        this->monitor->add(SetEvent(SetOperator::Remove, elem, result));
+        return result;
+    }
+
+
+    bool ctn(int elem) override {
+        bool result = set.ctn(elem);
+        // A02: Add code to check if the element is inside the set and update `result`.
+        this->monitor->add(SetEvent(SetOperator::Contains, elem, result));
+        return result;
+    }
+
+    void print_state() override {
+        // A02: Optionally, add code to print the state. This is useful for debugging,
+        // but not part of the assignment.
+        this->set.print_state();
     }
 };
